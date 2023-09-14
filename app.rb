c5b59c5e6c2c26004ff09d9423a51409ef88c624
Yosuke Motosugi <y.motosugi0227@gmail.com>
2023-09-14 23:58:48 +1000

    Refactor the method

--- app.rb
+++ app.rb
@@ -1,19 +1,13 @@
 def get_sub_strings(word, words)
   result = Hash.new(0)
-  sub_string = ""
-  word = word.downcase
-  for i in 0..(word.length - 1) do
-    for j in i..(word.length - 1) do
-      sub_string = i == j ? word[i] : word[i...j + 1]
-      if words.include?(sub_string) == true
-        result[sub_string] += 1
-      end
-    end
+  downcase_word = word.downcase
+
+  words.each do |word|
+    sub_string_count = downcase_word.scan(word).length
+    result[word] = sub_string_count unless sub_string_count == 0
   end
   result
 end
 
-
-# dictionary = ["a", "ab", "abc", "b", "bc", "c"]
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 p get_sub_strings("Howdy partner, sit down! How's it going?", dictionary)
