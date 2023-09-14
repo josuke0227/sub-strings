87d118d7eceea362de218915386ac4d1bf4f159b
Yosuke Motosugi <y.motosugi0227@gmail.com>
2023-09-15 00:06:51 +1000

    Get rid of default value from the hash

--- app.rb
+++ app.rb
@@ -1,11 +1,12 @@
 def get_sub_strings(word, words)
-  result = Hash.new(0)
+  result = {}
   downcase_word = word.downcase
 
   words.each do |word|
     sub_string_count = downcase_word.scan(word).length
     result[word] = sub_string_count unless sub_string_count == 0
   end
+  
   result
 end
 
